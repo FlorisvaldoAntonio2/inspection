c6b3542921e3052339fcb329efcecef38f61d7eb
Florisvaldo <florisvaldoantonio2@gmail.com>
2024-10-27 11:43:25 -0300

    FIX: uso do js do bootstrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,4 @@
-import 'bootstrap';
+import * as bootstrap from 'bootstrap';
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -7,6 +7,7 @@ import 'bootstrap';
  */
 
 import axios from 'axios';
+window.bootstrap = bootstrap;
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
